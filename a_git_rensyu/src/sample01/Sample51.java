cec14817b8c75d867e4e5fae80f0d430246549f9
mitsuru-okubo <mitsuru-okubo@PMR500N>
2022-06-22 12:53:43 +0900

    merge finish

--- a_git_rensyu/src/sample01/Sample51.java
+++ a_git_rensyu/src/sample01/Sample51.java
@@ -4,7 +4,12 @@ public class Sample51 {
 
 	public static void main(String[] args) {
 		System.out.println("mitsuru51です。");
-		
+
+		int num = 10;
+		int ans = num * 5;
+		System.out.println(ans);
+
+
 		String name = "ウルトラマン";
 		System.out.println(name);
 
