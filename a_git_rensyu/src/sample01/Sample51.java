7bc5d017657dd9d18031877b45bdd79618ddbfd8
Mitsuru Okubo <okubomi@gmail.com>
2022-06-22 12:47:13 +0900

    Sample51.java 追記しました。by okubomi

--- a_git_rensyu/src/sample01/Sample51.java
+++ a_git_rensyu/src/sample01/Sample51.java
@@ -4,7 +4,9 @@ public class Sample51 {
 
 	public static void main(String[] args) {
 		System.out.println("mitsuru51です。");
-
+		int num = 10;
+		int ans = num * 5;
+		System.out.println(ans);
 	}
 
 }
